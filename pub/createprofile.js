e4af1f8fa625b56119a2b402533a7dafede16a61
alulynn <alulynn@gmail.com>
2020-09-30 11:44:44 -0700

    more skeleton for createprofile.js

--- pub/createprofile.js
+++ pub/createprofile.js
@@ -6,9 +6,31 @@ function authStateObserver(user) {
 
   if (user) {
     // User is signed in.
-    window.alert("Welcome back, " + user.displayName + "!");   // set pop up to see if it's getting here.
+    //window.alert("Welcome back, " + user.displayName + "!");   // set pop up to see if it's getting here.
 
   } else {
     // No user is signed in.
   }
+}
+
+function createProfile() {
+    var user = firebase.auth().currentUser;
+    if (user) {
+        // Get data from html
+        var firstname = "";
+        var lastname = "";
+        var bio = "";
+        var height = "";
+        var weight = "";
+        var sex = "";
+
+        // stick data into json
+
+        // do validation
+
+        // send to firestore
+
+    } else {
+        // how did they make it here without being logged in
+    }
 }
