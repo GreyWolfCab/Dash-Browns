d217da8d1b6257c3b191660288886c7666b4a792
aimilbc <touyamaaimi927@gmail.com>
2020-10-07 09:23:45 -0700

    deleted variables for user profile

--- pub/main.js
+++ pub/main.js
@@ -8,8 +8,6 @@ var userOverlayDivElement = document.getElementsByClassName("user-overlay")[0];
 createAccountButtonElement.addEventListener("click", changeOverlay);
 userInfoSubmitButton.addEventListener("click", changeOverlay);
 
-
-
 function changeOverlay() {
     if (userOverlayDivElement.style.display === "none") {
         userOverlayDivElement.style.display = "block";
@@ -34,26 +32,5 @@ function changeOverlay() {
             // [END_EXCLUDE]
 
           });
-
-        console.log('account created for ' + fName);
-
-        // I'm going to try to initialize below info for the active user : Aimi =====================10/03
-        const fName = document.getElementById("firstName").value;
-        const lName = document.getElementById("lastName").value;
-        const bio = document.getElementById("bio").value;
-        const height = document.getElementById("height").value;
-        const weight = document.getElementById("weight").value;
-        const gender = document.getElementsByName("gender");
-        // get the value of the selected radio button
-        for(var i = 0; i < gender.length; i++) {
-          if(gender[i].checked)
-          {
-            const sex = gender[i].value;
-          }
-        }
-        
-        
-
-        
     }
 }
