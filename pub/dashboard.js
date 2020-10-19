54bc425b2a752c329c15759969b1cade3471d693
GreyWolfCab <54644740+GreyWolfCab@users.noreply.github.com>
2020-10-18 20:37:38 -0700

    Load user data into dashboard

--- pub/dashboard.js
+++ pub/dashboard.js
@@ -3,28 +3,56 @@
 //firebase DOM elements
 var signInButtonElement = document.getElementById("logout");
 
+//user referential elements
+var userNameElement = document.getElementById("user-fullname");
+var userWeightElement = document.getElementById("user-weight");
+var userHeightElement = document.getElementById("user-height");
+var userSexElement = document.getElementById("user-sex");
+var userGreetingElement = document.getElementById("firstName");
+
 //login listener
 signInButtonElement.addEventListener("click", signOut);
+
+//firestore references
+const usersReference = firebase.firestore().collection("Users");
 
 initFirebaseAuth();//upon loading check the auth status of the user
 
 function authStateObserver(user) {
   
-    var user = firebase.auth().currentUser;//authentication observer that holds user details
+  var user = firebase.auth().currentUser;//authentication observer that holds user details
   
-    if (user) {
+  if (user) {//user successfully logged in
 
-        console.log(user.uid);
-        console.log(user.email);
-        console.log(user.displayName);
-  
-    } else {
-      // No user is signed in.
-      window.alert("Please login again.");
-      //send the user to the login page
-        window.location.href = "login.html";
-    }
+    getUserInfo();
+
+  } else {
+    // No user is signed in.
+    window.alert("Please login again.");
+    //send the user to the login page
+    window.location.href = "login.html";
   }
+}
+
+function getUserInfo() {
+
+  usersReference.doc(getUserId()).get()
+  .then(function (querySnapshot) {
+    if (querySnapshot.exists) {
+      userNameElement.textContent = querySnapshot.data().firstname + " " + querySnapshot.data().lastname;
+      userWeightElement.textContent = "Weight: " + querySnapshot.data().weight + " lbs";
+      userHeightElement.textContent = "Height: " + Math.floor(querySnapshot.data().height / 12) + "ft " + querySnapshot.data().height % 12 + "in";
+      userSexElement.textContent = "Sex: " + querySnapshot.data().sex;
+      userGreetingElement.textContent = querySnapshot.data().firstname + ",";
+    } else {
+      //user doesn't exist
+    }
+  })
+  .catch(function (error) {
+    console.log("Error getting User information:", error);
+  });
+
+}
 
 // Signs-out of Dash Browns.
 function signOut() {
@@ -32,8 +60,17 @@ function signOut() {
     firebase.auth().signOut();
 }
 
+// Returns the signed-in user's ID.
+function getUserId() {
+  return firebase.auth().currentUser.uid;
+}
+
 // Initiate Firebase Auth.
 function initFirebaseAuth() {
     // Listen to auth state changes.
     firebase.auth().onAuthStateChanged(authStateObserver);
-  }
+}
+
+function User() {
+
+}
